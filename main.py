0483fa5c166f588d0237233724a6546de6207da9
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-02-23 22:00:47 +0700

    komentar

---
+++ main.py
@@ -0,0 +1,11 @@
+from array import *
+nilai = array('i',[1,2,3])
+nilai.append(4)
+for i in (nilai):
+    print(i)
+
+print("-----------------batas------------")
+
+nama = ["merry","wulan","dari"]
+for n in (nama):
+    print(n)
